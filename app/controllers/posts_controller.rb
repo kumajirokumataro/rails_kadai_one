753837850c1bcbcc313bde51e8b68984df0481e7
はらなおみ <haraweb7@gmail.com>
2023-10-02 10:30:09 +0900

    新規投稿後、保存機能、confirmへの遷移完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,10 +12,23 @@ class PostsController < ApplicationController
     render :new if @post.invalid?
   end
 
+  def create
+    @post = Post.new(post_params)
+    if params[:back] 
+      render :new
+    else
+      if @post.save
+        redirect_to posts_path, notice: "つぶやきを作成しました！"
+      else
+      render :new
+      end
+    end
+  end
+
   private
 
   def post_params
-    params.require(:post).permit(:updated_at, :content)
+    params.require(:post).permit(:content)
   end
 
   def set_post
