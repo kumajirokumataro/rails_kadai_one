11113fdf846a184903147350e0569b22f47052f6
はらなおみ <haraweb7@gmail.com>
2023-10-02 10:06:50 +0900

    privateメソッド、confirmメソッドの完成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,19 @@ class PostsController < ApplicationController
     @post = Post.new
   end
 
+  def confirm 
+    @post = Post.new(post_params)
+    render :new if @post.invalid?
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:updated_at, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
 end
