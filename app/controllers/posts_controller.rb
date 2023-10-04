5ebb158aa503311fd0318f20d1da9982b49b0c73
はらなおみ <haraweb7@gmail.com>
2023-10-04 12:56:40 +0900

    編集機能完成　バリデーション設定完了

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,19 +29,30 @@ class PostsController < ApplicationController
   def edit
   end
 
+  def show
+  end
+
   def destroy
     @post.destroy
     redirect_to posts_path, notice:" つぶやきを削除しました！"
   end
 
-  private
-
-  def post_params
-    params.require(:post).permit(:content)
+  def update
+    if @post.update(post_params)
+      redirect_to posts_path, notice: "つぶやきを編集しました！"
+    else
+      render :edit
+    end
   end
 
-  def set_post
-    @post = Post.find(params[:id])
-  end
+    private
+
+    def post_params
+      params.require(:post).permit(:content)
+    end
+
+    def set_post
+      @post = Post.find(params[:id])
+    end
 
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,4 @@
 class Post < ApplicationRecord
+  validates :content, length: { in: 1..75 } 
+  validates :content, presence: true
 end
